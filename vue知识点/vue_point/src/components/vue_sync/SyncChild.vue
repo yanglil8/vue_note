10b9863208a48e69c243290b783965b25f8910ac
yanglil8 <1653717386@qq.com>
2022-05-21 02:27:13 +0800

    11

---
+++ vue知识点/vue_point/src/components/vue_sync/SyncChild.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="children">
+    <h1>{{age}}</h1>
+    <input type="text"  :value=$emit(str)>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['age','str']
+};
+</script>
+
+<style>
+</style>  
---
+++ vue知识点/vue_point/src/components/vue_sync/SyncFather.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="father">
+    <sync-child :age = "age" :str = "str"/>
+    <button @click="add($event)">按钮</button>
+    <hr>
+    <input type="text"  v-model = "str">
+  </div>
+</template>
+
+<script>
+import SyncChild from './SyncChild.vue'
+
+export default {
+  components: {SyncChild },
+  //组件中定义data属性 只能是函数 
+  // data 对象就是要渲染到页面上的内容
+  data(){
+    return{
+      age :8,
+      str:'性张力'
+    }
+
+  },
+  methods:{
+    add(e){
+     console.log(e.target);// 事件对象的目标
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
